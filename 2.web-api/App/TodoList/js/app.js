d42a599bb4bc5dd15d5ab17edb4a8bbbdac6e59d
jh080724 <tsj010813@gmail.com>
2024-09-12 01:16:59 +0900

    수요일 숙제-main.css 92%, app.js 시작

---
+++ 2.web-api/App/TodoList/js/app.js
@@ -0,0 +1,24 @@
+// const $tmp=document.querySelector('.checkbox');
+// console.log($tmp);
+
+// const $submit = document.getElementById('add')
+// $submit.onmouseover = sayHelloHandler();
+
+// function sayHelloHandler(){
+//   alert('Hello~~~');
+// }
+
+// const $b1 = document.querySelector('.lnr-plus-circle');
+// // $b1.onmouseover = sayHelloHandler;
+// $b1.onclick = () => {
+//   $b1.style.background = 'yellow';
+// }
+
+// const $b2 = document.getElementById('add');
+// $b2.onmouseover = () => {
+//   $b2.style.background = 'white';
+// } 
+
+// $b2.onmouseout = () => {
+//   $b2.style.background = '#868e96';
+// }
